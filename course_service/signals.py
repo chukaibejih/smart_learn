f94c3e794db4ea907cfe32edf8984c459484015f
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-04-13 01:13:38 +0100

    used signals to control average rating and number of reviews in course model when a review is created, updated and deleted

--- course_service/signals.py
+++ course_service/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Review, Course
-from django.shortcuts import get_object_or_404
 from django.db.models import Avg
 
 @receiver([post_save, post_delete], sender=Review)
