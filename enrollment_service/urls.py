3537ff26cf770b1c346323daebe0217bb02091b9
Ibejih <chukaibejih@gmail.com>
2023-05-07 15:55:37 +0100

    Changed enrollment url

--- enrollment_service/urls.py
+++ enrollment_service/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import EnrollmentView, StripeView
 
 urlpatterns = [
-    path('', EnrollmentView.as_view(), name='enroll'),
+    path('', EnrollmentView.as_view(), name='enrol'),
     path('pay/', StripeView.as_view(), name='pay')
 ]
