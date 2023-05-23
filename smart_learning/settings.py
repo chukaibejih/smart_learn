15f999ad2c9edb312e67a7acd3354cfea913cb6f
Ibejih <chukaibejih@gmail.com>
2023-05-23 13:09:35 +0100

    ALLOWED_HOSTS

--- smart_learning/settings.py
+++ smart_learning/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY'),
 
-ALLOWED_HOSTS = ['https://smart-learn.onrender.com']
+ALLOWED_HOSTS = ['smart-learn.onrender.com']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
