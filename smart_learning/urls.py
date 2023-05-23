0da6db9f659fb1013e8988663c6bedb30b7e74bb
Ibejih <chukaibejih@gmail.com>
2023-05-23 13:18:36 +0100

    debug toolbar

--- smart_learning/urls.py
+++ smart_learning/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('enrol/', include('enrollment_service.urls')),
 
 
-    path('__debug__/', include('debug_toolbar.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
