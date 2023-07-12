436638ce6dbfea5e698aa41dba47150903261cb1
mayal <developer.web.dj@gmail.com>
2023-07-12 16:10:56 +0400

    UPDATE to rest_framework_simplejwt JWTAuthentication authentication only

--- smart_learning/settings.py
+++ smart_learning/settings.py
@@ -84,7 +84,6 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (       
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
