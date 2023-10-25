320f4db9e4d939cf95140365633913d12fac8854
TaskMasterErnest <kluernest08@gmail.com>
2023-10-25 15:36:49 +0000

    restored to original state

--- build.sh
+++ build.sh
@@ -2,18 +2,11 @@
 
 set -o errexit  # exit when there is an error error
 
-# provide the superuser email, or a default
-SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"hello@ernest.com"}
-
-cd /api/
-
-# /opt/venv/bin/pip install -r requirements.txt
+pip install -r requirements.txt
 
 # python manage.py collectstatic --no-input
-/opt/venv/bin/python manage.py migrate --noinput
+python manage.py migrate
 
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
-
-# if [[ $CREATE_SUPERUSER ]]; then
-#   /opt/venv/bin/python manage.py createsuperuser --no-input
-# fi
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
+fi
