6984ee1baef0d409e461ea96f1b1963e9635cb53
TaskMasterErnest <kluernest08@gmail.com>
2023-10-25 15:40:04 +0000

    modified settings to work with new configuration

--- smart_learning/settings.py
+++ smart_learning/settings.py
@@ -128,14 +128,14 @@ DATABASES = {
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
 
-    # STAGING
+    # DEVELOPMENT
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("POSTGRESQL_DATABASE"),
-        'USER': os.environ.get("POSTGRESQL_USERNAME"),
-        'PASSWORD': os.environ.get("POSTGRESQL_PASSWORD"),
-        'HOST': os.environ.get("POSTGRESQL_HOST", 'localhost'),
-        'PORT': os.environ.get("POSTGRESQL_PORT", 5432),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),
+        'PORT': os.environ.get("POSTGRES_PORT", 5432),
     }
     
  #PRODUCTION
